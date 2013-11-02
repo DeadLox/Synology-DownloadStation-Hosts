b6714e60db87d10684c825a1f01179a12f335d7d
mcampbell508 <matthewcampbell508@gmail.com>
2013-11-02 13:17:33 +0000

    Update host.php

--- oteupload/host.php
+++ oteupload/host.php
@@ -5,14 +5,17 @@
 * credits to metter with rapidgator host file
 */
 
-
+define('HTTPHEADER', "Accept-Language: en-us,en;");  // default lang is russian
+//require('../common.php');
 class SynoFileHostingOteupload {
 	private $Url;
 	private $Username;
 	private $Password;
 	private $HostInfo;
 	private $OTE_COOKIE_JAR = '/tmp/oteupload.cookie';
+	//private $OTE_COOKIE_JAR = 'oteupload.cookie';
 	private $LOGIN_URL = "https://www.oteupload.com/login.php";
+	private $ACCOUNT_URL = "https://www.oteupload.com/my_account.php";
 		
 	public function __construct($Url, $Username, $Password, $HostInfo) {
 		$this->Url = $Url;
@@ -29,6 +32,11 @@ class SynoFileHostingOteupload {
 		if($this->performLogin()==LOGIN_FAIL) {
 			$DownloadInfo = array();
 			$DownloadInfo[DOWNLOAD_ERROR] = ERR_REQUIRED_PREMIUM;
+			return $DownloadInfo;
+		}
+		if($this->checkAccountTraffic()==ERR_TRY_IT_LATER) {
+			$DownloadInfo = array();
+			$DownloadInfo[DOWNLOAD_ERROR] = ERR_TRY_IT_LATER;
 			return $DownloadInfo;
 		}
 		return $this->getPremiumDownloadLink();
@@ -59,7 +67,7 @@ class SynoFileHostingOteupload {
 		
 		//echo $LoginInfo;
 		
-		//xfss is oteupload logged in cookie value
+		//xfss is filefactory logged in cookie value
 		if (FALSE != $LoginInfo && file_exists($this->OTE_COOKIE_JAR)) {
 			$cookieData = file_get_contents ($this->OTE_COOKIE_JAR);
 			if(strpos($cookieData,'xfss') !== false) {
@@ -74,23 +82,56 @@ class SynoFileHostingOteupload {
 		return $ret;
 	}
 	
+	public function checkAccountTraffic(){
+		$ret = FALSE;	
+		$curl1 = curl_init();	
+		curl_setopt($curl1, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($curl1, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
+		curl_setopt($curl1, CURLOPT_COOKIEFILE, $this->OTE_COOKIE_JAR);
+		curl_setopt($curl1, CURLOPT_COOKIEJAR, $this->OTE_COOKIE_JAR);
+		curl_setopt($curl1, CURLOPT_RETURNTRANSFER, TRUE);  // return page content	
+		curl_setopt($curl1, CURLOPT_URL, $this->ACCOUNT_URL);
+		curl_setopt($curl1, CURLOPT_HTTPHEADER, array(HTTPHEADER));
+		curl_setopt($curl1, CURLOPT_HEADER, FALSE); // set TRUE displays Header for debug purposes	
+		$AccountRet = curl_exec($curl1);		
+		curl_close($curl1);
+		
+		//var_dump($AccountRet);
+		if (strstr($AccountRet, "0MB</div>")) {
+			//echo "TRAFFIC UP";
+			$ret = ERR_TRY_IT_LATER;
+			return $ret;
+		}else{
+			//echo "good";
+			$ret = USER_IS_PREMIUM;
+			return $ret;
+		}
+		
+		$ret = ERR_TRY_IT_LATER;
+		return $ret;
+	}
+	
 	private function getPremiumDownloadLink() {
 		
 		$ret = false;
 		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
 		curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
 		curl_setopt($curl, CURLOPT_URL, $this->Url);
 		curl_setopt($curl, CURLOPT_COOKIEFILE, $this->OTE_COOKIE_JAR);
+		curl_setopt($curl, CURLOPT_COOKIEJAR, $this->OTE_COOKIE_JAR);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
-		
-		curl_setopt($curl, CURLOPT_HEADER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array(HTTPHEADER));
+		curl_setopt($curl, CURLOPT_HEADER, FALSE);
 		//curl exec has to be called before getinfo
 		$header = curl_exec($curl);
 		$code = curl_getinfo($curl, CURLINFO_REDIRECT_URL);
 		$info = curl_getinfo($curl);
 		$error_code = $info['http_code'];
 		
-		//var_dump(curl_getinfo($curl));
+		
+		
+		//var_dump($header);
 		//echo $code;
 		//if 302 found in header - file is working and downloadable
 		if ($error_code == 301 || $error_code == 302) { 
@@ -100,7 +141,8 @@ class SynoFileHostingOteupload {
 			return $DownloadInfo;
 		}else{
 			//echo $header;
-			//echo "error";
+			//echo "error no file exists";
+			//echo (strstr($header, ">0MB</div>"));
 			//var_dump(curl_getinfo($curl));
 			$DownloadInfo = array();
 			$DownloadInfo[DOWNLOAD_ERROR] = ERR_FILE_NO_EXIST;
@@ -112,6 +154,8 @@ class SynoFileHostingOteupload {
 
 //$t = new SynoFileHostingOteupload('https://www.oteupload.com/n9mh5rcfb88r/encantadoras.cap.39.mp4.html', '', '', '');
 //$t->Verify();
+
+//$t->getPremiumDownloadLink();
 //$t->getPremiumDownloadLink();
 
 ?>
